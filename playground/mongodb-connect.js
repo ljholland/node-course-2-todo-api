ea761395174db038d11484e89358624cd559b4f3
VM User <vmuser@bah.com>
2016-11-07 11:58:08 -0500

    Added update

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,30 +7,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log ('Connected to Mongodb');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, results) => {
-  //   if (err) {
-  //     return console.log ('Unable to insert Todo');
-  //   }
-  //   console.log(JSON.stringify(results.ops, undefined, 2));
-  // });
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, results) => {
+    if (err) {
+      return console.log ('Unable to insert Todo');
+    }
+    console.log(JSON.stringify(results.ops, undefined, 2));
+  });
 
   // insert new doc into users (name, age, location)
   //
-  db.collection('Users').insertOne({
-      name: 'Lance Holland',
-    age: 33,
-    location: 'Troy'
-  }, (err, results) => {
-    if (err) {
-      return console.log ('Unable to insert Users');
-    }
-    // console.log(JSON.stringify(results.ops, undefined, 2));
 
-    // console.log(results.ops[0]._id.getTimestamp());
-  });
+
+  // db.collection('Users').insertOne({
+  //     name: 'Lance Holland',
+  //   age: 33,
+  //   location: 'Troy'
+  // }, (err, results) => {
+  //   if (err) {
+  //     return console.log ('Unable to insert Users');
+  //   }
+  //   // console.log(JSON.stringify(results.ops, undefined, 2));
+  //
+  //   // console.log(results.ops[0]._id.getTimestamp());
+  // });
+
 
   db.close();
 });
