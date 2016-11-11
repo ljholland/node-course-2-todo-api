5f4051205f1866129bca78de4c84d248aa033053
VM User <vmuser@bah.com>
2016-11-11 08:35:37 -0500

    Adding user auth

--- server/server.js
+++ server/server.js
@@ -96,6 +96,18 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+app.post('/users', (req, res) => {
+  var body =  _.pick(req.body, ['email','password']);
+
+  var user = new User(body);
+
+  user.save().then((doc) => {
+    res.send(user);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
