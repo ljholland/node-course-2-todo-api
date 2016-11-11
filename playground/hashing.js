10c3d78afda7af9ef77fd6171365f06dfadb1d42
VM User <vmuser@bah.com>
2016-11-11 14:22:55 -0500

    Added password encryption

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+//
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var data = {
-  id: 10
-};
+var hashedPassword = '$2a$10$fJ/6gTzxf2quS5LwPFq89e701ExZNWPpFFi9np5/hYLstdGHk8YvW';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 //
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
